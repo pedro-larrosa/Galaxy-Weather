2933b67239339dea4971d6c7333f2728a4ccbb95
Pedro <larrosacasellespedro@gmail.com>
2024-08-23 02:36:34 +0200

    Detalle de el tiempo

--- src/config.js
+++ src/config.js
@@ -3,5 +3,3 @@ export const WEATHER_URL = `${import.meta.env.VITE_APP_API_DOMAIN}${import.meta.
 export const GEO_URL = `${import.meta.env.VITE_APP_API_DOMAIN}${import.meta.env.VITE_APP_API_GEO_BASE}` //https://api.openweathermap.org/geo/1.0/direct
 
 export const API_KEY = `${import.meta.env.VITE_API_KEY}`
-
-console.log(import.meta.env.VITE_APP_API_DOMAIN)
